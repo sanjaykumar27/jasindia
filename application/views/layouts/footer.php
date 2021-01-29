a2f2ce0a096107a6e2c914b60f635dd43f16fe62
Sanjay <sanjaychaurasia@slashash.co>
2021-01-29 16:29:41 +0530

    bug

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -2,7 +2,7 @@
 		 <script src="https://code.jquery.com/jquery-3.5.1.min.js" ></script>
 		 <script src="<?php echo base_url();?>assets/js/bootstrap.js" ></script>
         <script src="<?php echo base_url();?>assets/custom.js" type="text/javascript"></script>
-        <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.19.0/jquery.validate.min.js"></script>
+        <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.19.0/jquery.validate.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     </body>
     <!-- end::Body -->
