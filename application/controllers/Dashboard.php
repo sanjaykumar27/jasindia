df1b69ff68c28c2e09e9ae13c0841a68660da4b6
Sanjay Kumar <engineer.sanjay20@gmail.com>
2021-03-19 19:59:02 +0530

    Script added for sentence case

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -23,6 +23,8 @@ class Dashboard extends CI_Controller {
     {
         parent::__construct();
         $this->load->model('Common_model');
+        $this->load->model('Master/State_model');
+		
 	}
 	
 	public function index()
@@ -37,5 +39,78 @@ class Dashboard extends CI_Controller {
             redirect('/Auth');
         }
 		
+	}
+
+	public function setState() {
+		$records = $this->Common_model->SelectAll('m_states');
+		foreach($records as $value)
+		{
+			$param = array(
+				'state_name' => ucwords(strtolower(trim($value->state_name)))
+			);
+			$cid = $this->State_model->updateState($param, $value->state_id);
+		}
+		echo 'DONE';
+	}
+
+	public function setDistrict() {
+		$records = $this->Common_model->SelectAll('m_districts');
+		foreach($records as $value)
+		{
+			$param = array(
+				'district_name' => ucwords(strtolower(trim($value->district_name)))
+			);
+			$cid = $this->State_model->updateDistrict($param, $value->district_id);
+		}
+		echo 'DONE';
+	}
+
+	public function importData() {
+		$file = fopen("pincodes.csv","r");
+		while(! feof($file))
+		{
+			$data[] = fgetcsv($file);
+		}
+		
+		$records = $this->Common_model->SelectAll('m_states');
+		$records = json_decode(json_encode($records), true);
+		unset($data[0]);
+		
+		foreach($data as $key => $value) {
+			$districts[] = $value['2']."##".$value['3'];
+		}
+			
+		$districts = array_unique($districts);
+		
+		$i = 1;
+		foreach($districts as $key => $value)
+		{
+			foreach($records as $val)
+			{
+				$split = explode("##",$value);
+				if($split['1'] == $val['state_name'])
+				{
+					$param = array(
+						'state_id' => $val['state_id'],
+						'district_name' => $split['0'],
+						'created_by' => 1
+					);
+					$this->Common_model->CommonInsert('m_districts',$param);
+					echo '<pre>';print_r($i." record inserted.");
+					$i++;
+				}
+			}
+		}
+		/*foreach($states as $value) {
+		 	$param = array(
+		 		'state_name' => $value,
+		 		'created_by' => 1
+		 	);
+		 	$this->Common_model->CommonInsert('m_states',$param);
+		}*/
+		
+		
+
+		echo '<pre>';print_r("DONE");
 	}
 }
